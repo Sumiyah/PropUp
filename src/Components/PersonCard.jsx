6ada5e2b43e34466783860afee1aa8d13adaacd2
Sumiyah <sumiyah.fa@gmail.com>
2020-11-25 12:24:12 +0300

    Changing PersonCard to functional components

--- src/Components/PersonCard.jsx
+++ src/Components/PersonCard.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+/*import { Component } from 'react';
 
 
 class PersonCard extends Component {
@@ -31,6 +31,28 @@ class PersonCard extends Component {
       </div>
     );
   }
+}*/
+
+//--------------------------------ATTENTION!--------------------------------//
+
+// TO SHOW THE FUNCTIONAl COMPONENTS!
+
+import React from 'react';
+const PersonCard = props => {
+  
+  return (
+    <div className="col-sm-4 mb-5">
+      <div className="card tall">
+        <div className="card-header bg-danger text-light">{props.lastName}, {props.firstName}</div>
+        <div className="card-body">
+          <p>Age: {props.age}</p>
+          <p>Hair Color: {props.hairColor}</p>
+          
+          {/* <button className="btn btn-outline-success btn-block at-bottom mt-auto mb-0" onClick={ageUp}>Birthday Button for {props.firstName} {props.lastName}</button> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PersonCard;
